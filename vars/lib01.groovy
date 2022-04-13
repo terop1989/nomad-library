db2a751cbd2f3a10e59b76c28dd56c50133fedb0
Yuriy Kozhin <YKozhin@IBS.RU>
2022-04-13 17:10:44 +0300

    first commit

---
+++ vars/lib01.groovy
@@ -0,0 +1,27 @@
+def call body {
+
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    throttleCategory = 'example'
+
+    throttle([throttleCategory]) {
+
+        node('master') {
+
+            stage('Checkout SCM') {
+                checkout scm
+                echo "Branch name is ${env.BRANCH_NAME}\nTag name is ${env.TAG_NAME}"
+            }
+
+            stage('Cleanup') {
+                deleteDir()
+            }
+
+        }
+
+    }
+
+}
